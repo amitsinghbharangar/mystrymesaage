07ec8dacc7280a02ddd067b88f4d6709535e8e26
Amit Singh <amitsinghbharangar001@gmail.com>
2024-12-25 10:18:14 +0530

    signup added

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import {resend} from '@/lib/resend';
+import VerificationEmail from '../../emails/VerificationEmail';
+VerificationEmail;
+import { ApiResponse } from '@/types/ApiResponse';
+
+export async function sendVerificationEmail(
+    email:string,
+    userName:string,
+    verifyCode:string,
+): Promise<ApiResponse>{
+    try{
+        const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: email,
+        subject: 'Mystry message | Verification code',
+        react: VerificationEmail({userName,otp:verifyCode})
+        });
+        return{success:true, message:"Verification email send successfully."}
+    }catch (emailError){
+        console.error("Error sending verification email",emailError)
+        return{success:false, message:"Failed to send verification email."}
+    }
+}
--- src/model/User.ts
+++ src/model/User.ts
@@ -66,4 +66,4 @@ const UserSchema : Schema<User> = new Schema({
     messages:[MessageSchema]
 })
 
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema)
+export const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema)
